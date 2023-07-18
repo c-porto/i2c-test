fb90abc0fa2ae6c76e30a3f9e46bcd5119e6949a
c-porto <gut0@DESKTOP-37PJH7E>
2023-07-18 18:08:27 -0300

    Finished i2c registers definitions

---
+++ test.c
@@ -0,0 +1,9 @@
+#include "test.h"
+
+void test_setup(void){
+    //configure led pin
+}
+
+void test_run(void){
+    //handle i2c using led interactions
+}
--- test.h
+++ test.h
@@ -1,9 +1,25 @@
-// I2C definitions
-#define I2C_FLAG            
-#define I2C_START_FLAG      
-#define I2C_STOP_FLAG       
-#define I2C_RX_FLAG         
-#define I2C_TX_FLAG         
-#define I2C_RX_BUFFER       
-#define I2C_TX_BUFFER       
+#ifndef TEST_H_
+#define TEST_H_
 
+// External includes
+#include <msp430.h>
+#include <stdint.h>
+#include "misc_def.h"
+
+// I2C definitions
+#define I2C_FLAG            UCB1IFG
+#define I2C_START_FLAG      UCSTTIFG
+#define I2C_STOP_FLAG       UCSTPIFG
+#define I2C_RX_FLAG         UCRXIFG0
+#define I2C_TX_FLAG         UCTXIFG0
+#define I2C_RX_BUFFER       UCB1RXBUF_L
+#define I2C_TX_BUFFER       UCB1TXBUF_L
+
+//LED definitions
+#define LED
+
+//task functions
+void test_setup(void);
+void test_run(void);
+
+#endif // !TEST_H_
