7b307794c781876b47d636480a0044dcc511d963
c-porto <gut0@DESKTOP-37PJH7E>
2023-07-21 15:40:16 -0300

    Initial progress of testing structure

--- test.c
+++ test.c
@@ -1,9 +1,44 @@
 #include "test.h"
+#include <stdint.h>
+
+void ISR(void) {
+
+    // Start
+    if (I2C_FLAG & I2C_START_FLAG) {
+        ptr_command  = (uint8_t *)command_bfr;
+        BIT_CLEAR(I2C_FLAG, I2C_START_FLAG);
+    }
+
+    // RX
+    if (I2C_FLAG & I2C_RX_FLAG) {
+        *ptr_command++ = I2C_RX_BUFFER;         //Receive the commands from master.
+        cmd_counter++;
+    }
+
+
+    // Stop
+    if(I2C_FLAG & I2C_STOP_FLAG) {
+        BIT_CLEAR(I2C_FLAG, I2C_STOP_FLAG);
+    }
+}
 
 void test_setup(void){
-    //configure led pin
+    #if defined(DEBUG_MODE) && (DEBUG_MODE == 0)
+         #define UART_MODE
+        // configure UART
+    #endif    
 }
 
 void test_run(void){
-    //handle i2c using led interactions
+    // Run just the debug
+    while (1) {
+        #if defined (UART_MODE)
+            //UART stuff
+        #endif 
+        ISR();
+        if (cmd_counter >= MAX_CMD) {                   // Needed to avoid going out of buffer length
+            ptr_command = (uint8_t *) command_bfr;      // or &command_bfr[0];
+        }
+        //delay maybe?
+    }
 }
--- test.h
+++ test.h
@@ -6,6 +6,10 @@
 #include <stdint.h>
 #include "misc_def.h"
 
+//Conditional macros
+#define DEBUG_MODE          0
+#define MAX_CMD             10
+
 // I2C definitions
 #define I2C_FLAG            UCB1IFG
 #define I2C_START_FLAG      UCSTTIFG
@@ -15,11 +19,16 @@
 #define I2C_RX_BUFFER       UCB1RXBUF_L
 #define I2C_TX_BUFFER       UCB1TXBUF_L
 
-//LED definitions
-#define LED
+// UART definitions
+
+//Variables
+uint8_t command_bfr[MAX_CMD];                   // Store the commands received from I2C
+uint8_t *ptr_command;                           // Receive the commands by the Rx Buffer of I2C.
+uint8_t cmd_counter = 0;                            // Used to track number of commands
 
 //task functions
 void test_setup(void);
 void test_run(void);
+void ISR(void);
 
 #endif // !TEST_H_
