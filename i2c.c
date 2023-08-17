088f3003e3254b81e63079eb0ac35734d0c8fb40
c-porto <gut0@DESKTOP-37PJH7E>
2023-08-16 22:03:19 -0300

    Configuring missing registers

--- i2c.c
+++ i2c.c
@@ -9,14 +9,18 @@ void i2c_setup(void) {
   BIT_SET(SDA_OUT, SDA_PIN); // Set resistor to pull-up on SDA.
   BIT_SET(SCL_OUT, SCL_PIN); // Set resistor to pull-up on SCL.
 
-  BIT_SET(SDA_SEL, SDA_PIN); // Assign I2C SDA to USCI_B0.
-  BIT_SET(SCL_SEL, SCL_PIN); // Assign I2C SCL to USCI_B0.
+  BIT_CLEAR(SDA_SEL0, SDA_PIN); // Configuring pins
+  BIT_CLEAR(SDA_SEL0, SDA_PIN); // Configuring pins
+  BIT_SET(SDA_SEL1, SDA_PIN);   // Assign I2C SDA to USCI_B0.
+  BIT_SET(SCL_SEL1, SCL_PIN);   // Assign I2C SCL to USCI_B0.
 
   BIT_SET(I2C_CONTROL_0, I2C_SW_RESET);   // Enable SW reset.
   BIT_SET(I2C_CONTROL_0, I2C_SLAVE_MODE); // I2C Slave.
   BIT_SET(I2C_CONTROL_0, I2C_SYNC_MODE);  // Synchronous mode.
   BIT_SET(I2C_OWN_ADDRESS, SLAVE_ADDRESS_A);
+  BIT_SET(I2C_OWN_ADDRESS, ADDRESS0_ENABLE);
   BIT_CLEAR(I2C_CONTROL_0, I2C_SW_RESET); // Clear SW reset, resume operation.
+  BIT_CLEAR(P_INIT_REG, INIT_PIN);
 
   // I2C_EN_INTERRUPT |= UCSTPIE | UCSTTIE | UCRXIE | UCTXIE;    // Enable STT,
   // STP, TX & RX interrupt.
--- i2c.h
+++ i2c.h
@@ -11,18 +11,24 @@ void i2c_setup(void);
 #define I2C_SLAVE_MODE UCMODE_3
 #define I2C_SYNC_MODE UCSYNC
 #define I2C_EN_INTERRUPT UCB1IE
+#define ADDRESS0_ENABLE UCOAEN
 #define I2C_OWN_ADDRESS UCB1I2COA0
 #define SLAVE_ADDRESS_A 0x31
 #define SLAVE_ADDRESS_B 0x32
 
 #define SDA_REN P3REN
 #define SDA_OUT P3OUT
-#define SDA_SEL P3SEL1
+#define SDA_SEL0 P3SEL0
+#define SDA_SEL1 P3SEL1
 #define SDA_PIN BIT1
 
 #define SCL_REN P3REN
 #define SCL_OUT P3OUT
-#define SCL_SEL P3SEL1
+#define SCL_SEL0 P3SEL0
+#define SCL_SEL1 P3SEL1
 #define SCL_PIN BIT2
+
+#define INIT_PIN LOCKLPM5
+#define P_INIT_REG PM5CTL0
 
 #endif // !DEBUG
